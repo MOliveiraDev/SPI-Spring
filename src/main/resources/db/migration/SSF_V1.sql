b0180aba32d8c27687062ff558470cb2c0bcef43
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-03-15 15:08:20 -0300

    Create SSF_V1.sql

---
+++ src/main/resources/db/migration/SSF_V1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE CPF_dos_Usuarios (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    endereco VARCHAR(255),
+    estado VARCHAR(255),
+    cpf VARCHAR(255)
+);
