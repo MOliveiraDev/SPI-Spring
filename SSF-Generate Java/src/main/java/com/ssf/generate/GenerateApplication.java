9ae72b934c56f19f7a9d6fc381705678c3689cca
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-03-20 11:50:20 -0300

    Update GenerateApplication.java

--- SSF-Generate Java/src/main/java/com/ssf/generate/GenerateApplication.java
+++ SSF-Generate Java/src/main/java/com/ssf/generate/GenerateApplication.java
@@ -2,14 +2,17 @@ package com.ssf.generate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class GenerateApplication {
 
-	public static void main(String[] args) {
-		// Carregar a variável do .env
-		Dotenv dotenv = Dotenv.load();
-		SpringApplication.run(GenerateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Carregar as variáveis do .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(GenerateApplication.class, args);
+    }
 
 }
