9e1ed0e3b3ac8456793d8f6466139ff6004e14a0
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-03-27 17:31:17 -0300

    Post

--- SSF-Generate Java/src/main/java/com/ssf/generate/Repository/UsuarioFinalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssf.generate.Repository;
-
-import com.ssf.generate.Model.UsuarioFinalModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioFinalRepository extends JpaRepository<UsuarioFinalModel, Long> {
-        UsuarioFinalModel findByEmail(String email);
-        UsuarioFinalModel findByTokenRecuperacao(String tokenRecuperacao);
-}
